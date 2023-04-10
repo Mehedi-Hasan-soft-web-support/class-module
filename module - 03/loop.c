fb56d4c50ad7506422ccdd322e91c11f0ce168c1
Md.Mehedi Hasan <129537430+Mehedi-Hasan-soft-web-support@users.noreply.github.com>
2023-04-10 12:38:35 +0600

    Add files via upload

---
+++ module - 03/loop.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=5;i=i+1)
+    {
+        printf("Hellow world\n");
+    }
+    return 0;
+}
+
+        //    we can make it easy
+
+// #include<stdio.h>
+// int main()
+// {
+//     int i;
+//     for(i=1;i<=14;i=i+1)
+//     {
+//         printf("Hellow world\n");
+//     }
+//     return 0;
+// }
